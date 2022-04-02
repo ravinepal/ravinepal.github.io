212916d68b786b8b40d584cae1b3ec7839e5a29b
ravi <kumarav4@gmail.com>
2022-04-02 11:22:57 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
         <div className="header">
           <h1>Ravi Kumar, <a href="https://twitter.com/RaviNepal" rel="noreferrer" target="_blank">@RaviNepal</a></h1>
            <p className ="alignright">
-               <Link href="/"> Home </Link> <Link href="/speaking"> | Speaking </Link> <Link href="/blogs"> | Blog </Link> <Link href="/media"> | Media </Link>
+               <Link href="/"> Home </Link> <Link href="/speaking"> | Speaking </Link> <Link href="/blogs"> | Blog </Link> <Link href="/media"> | Media </Link></p>
         <div style={{clear: 'both'}}></div>
         </div>
     )
