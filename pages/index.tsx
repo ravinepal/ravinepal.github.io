7982809fb36eaffb7e0d1f916cb67f3495abf79e
Ishan Dongol <ishandongol@gmail.com>
2022-04-02 20:34:22 +0545

    Syntax error fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,102 @@
 /* eslint-disable @next/next/no-img-element */
-import type { NextPage } from 'next'
-import { Layout } from '../components/Layout'
+import type { NextPage } from "next";
+import { Layout } from "../components/Layout";
 const Home: NextPage = () => {
   return (
-      <Layout>
-        <br/> <br/>
-      <img width="200" height="200" className="img-circle avatar" alt="Ravi Kumar" src="/images/ravi.jpg" />
+    <Layout>
+      <img
+        width="200"
+        height="200"
+        className="img-circle avatar"
+        alt="Ravi Kumar"
+        src="/images/ravi.jpg"
+      />
       <section>
-        <div style={{fontWeight:'normal'}}>
-
-<p></p>
-<p>Ravi Kumar specializes in human capital development and creating data-driven and user-centered solutions to improve lives.</p> 
-          
-<p>At the World Bank, Ravi serves as Team Leader for the<a href="https://www.worldbank.org/dataliteracy" target="_blank"> Data Use and Literacy Program housed in the Development Data Group in Washington, DC.
-
-<p>In this role, he conceptualizes and manages the delivery of a range of activities to build capacity for data literacy and data use, enable data-driven decision-making, improve public services, and democratize participation in the data revolution across low- and middle-income countries. <p>He joined the World Bank in 2012 and most recently he led <a href="https://www.worldbank.org/en/news/video/2020/05/23/increasing-the-use-of-data-for-prosperous-nepal" target="_blank"> open data and data use </a> initiatives at the World Bank Nepal Office.</p>
-
-<p>His experience includes program and team management, digital and data strategy, research, publications, partnerships, advocacy, risk and stakeholder management across Africa, East Asia and Pacific, Europe and Central Asia, Middle East and North Africa, and South Asia.</p>
-
-<p>A champion of diversity, equity, and inclusion, he has served as a Diversity and Inclusion Advocate at the World Bank advising senior management on initiatives, monitoring trends, and sharing information on best practices.</p>
-
-<p>He is a member of the Society for Policy Sciences and was named one of the Asia Society’s “Asia 21 Young Leaders” in 2018. He has served on Buena Vista University’s President’s Advisory Council and was an inaugural Penn Kemble Fellow at the National Endowment for Democracy.</p>
-
-<p>As a social entrepreneur, Ravi has founded and led two non-profits, including <a href="https://codefornepal.org/" target="_blank">Code for Nepal, </a>. In these ventures, he has helped make data accessible, increased digital and data literacy among thousands of youths, and rebuilt dozens of schools in Nepal. He holds a master’s degree from <a href="http://www.columbia.edu" target="blank"> Columbia University in New York </a> and a bachelor’s degree with honors from <a href="http://www.bvu.edu/" target="blank">Buena Vista University</a>.</p>
-
-<p>He lives with his wife in the greater Washington DC area, in a house powered by renewable energy.</p>
-
-<p>Connect with him on Twitter <a href="https://twitter.com/RaviNepal" target="_blank">@RaviNepal</a>.</p>
-</div>
+        <div style={{ fontWeight: "normal" }}>
+          <p>
+            Ravi Kumar specializes in human capital development and creating
+            data-driven and user-centered solutions to improve lives.
+          </p>
+          <p>
+            At the World Bank, Ravi serves as Team Leader for the
+            <a href="https://www.worldbank.org/dataliteracy" target="_blank">
+              {" "}
+              Data Use and Literacy Program{" "}
+            </a>{" "}
+            housed in the Development Data Group in Washington, DC.{" "}
+          </p>
+          <p>
+            In this role, he conceptualizes and manages the delivery of a range
+            of activities to build capacity for data literacy and data use,
+            enable data-driven decision-making, improve public services, and
+            democratize participation in the data revolution across low- and
+            middle-income countries.
+          </p>{" "}
+          <p>
+            He joined the World Bank in 2012 and most recently he led{" "}
+            <a
+              href="https://www.worldbank.org/en/news/video/2020/05/23/increasing-the-use-of-data-for-prosperous-nepal"
+              target="_blank"
+            >
+              {" "}
+              open data and data use{" "}
+            </a>{" "}
+            initiatives at the World Bank Nepal Office.
+          </p>
+          <p>
+            His experience includes program and team management, digital and
+            data strategy, research, publications, partnerships, advocacy, risk
+            and stakeholder management across Africa, East Asia and Pacific,
+            Europe and Central Asia, Middle East and North Africa, and South
+            Asia.
+          </p>
+          <p>
+            A champion of diversity, equity, and inclusion, he has served as a
+            Diversity and Inclusion Advocate at the World Bank advising senior
+            management on initiatives, monitoring trends, and sharing
+            information on best practices.
+          </p>
+          <p>
+            He is a member of the Society for Policy Sciences and was named one
+            of the Asia Society’s “Asia 21 Young Leaders” in 2018. He has served
+            on Buena Vista University’s President’s Advisory Council and was an
+            inaugural Penn Kemble Fellow at the National Endowment for
+            Democracy.
+          </p>
+          <p>
+            As a social entrepreneur, Ravi has founded and led two non-profits,
+            including{" "}
+            <a href="https://codefornepal.org/" target="_blank">
+              Code for Nepal,{" "}
+            </a>
+            . In these ventures, he has helped make data accessible, increased
+            digital and data literacy among thousands of youths, and rebuilt
+            dozens of schools in Nepal. He holds a master’s degree from{" "}
+            <a href="http://www.columbia.edu" target="blank">
+              {" "}
+              Columbia University in New York{" "}
+            </a>{" "}
+            and a bachelor’s degree with honors from{" "}
+            <a href="http://www.bvu.edu/" target="blank">
+              Buena Vista University
+            </a>
+            .
+          </p>
+          <p>
+            He lives with his wife in the greater Washington DC area, in a house
+            powered by renewable energy.
+          </p>
+          <p>
+            Connect with him on Twitter{" "}
+            <a href="https://twitter.com/RaviNepal" target="_blank">
+              @RaviNepal
+            </a>
+            .
+          </p>
+        </div>
+      </section>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
